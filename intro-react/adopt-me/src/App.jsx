ffe49f482da32d98fba5974aaf102b6d1ffc6d7b
pagarevijayy <pagarevj97@gmail.com>
2023-11-06 00:05:03 +0530

    react query

--- intro-react/adopt-me/src/App.jsx
+++ intro-react/adopt-me/src/App.jsx
@@ -1,18 +1,30 @@
 import { createRoot } from "react-dom/client";
-import SearchParams from "./SearchParams";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import SearchParams from "./SearchParams";
 import Details from "./Details";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
   return (
     <BrowserRouter>
-      <header>
-        <Link to="/">Adopt me!</Link>
-      </header>
-      <Routes>
-        <Route path="/details/:id" element={<Details />} />
-        <Route path="/" element={<SearchParams />} />
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/">Adopt me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
--- intro-react/adopt-me/src/Details.jsx
+++ intro-react/adopt-me/src/Details.jsx
@@ -1,10 +1,29 @@
 import { useParams } from "react-router-dom";
+import fetchPet from "./fetchPet";
+import { useQuery } from "@tanstack/react-query";
 
 function Details() {
   const { id } = useParams();
+  const results = useQuery(["details", id], fetchPet);
+
+  if (results.isLoading) {
+    return (
+      <div className="loading-pane">
+        <h2 className="loader">🌀</h2>
+      </div>
+    );
+  }
+
+  const pet = results.data.pets[0];
+
   return (
-    <div>
-      <h2>{id}</h2>
+    <div className="details">
+      <div>
+        <h1>{pet.name}</h1>
+        <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
+        <button>Adopt {pet.name}</button>
+        <p>{pet.description}</p>
+      </div>
     </div>
   );
 }
