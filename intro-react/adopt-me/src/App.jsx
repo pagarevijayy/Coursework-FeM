8aec824580c0dfe825435306801e2665c23cbf41
pagarevijayy <pagarevj97@gmail.com>
2023-11-05 21:46:37 +0530

    React Router

--- intro-react/adopt-me/src/App.jsx
+++ intro-react/adopt-me/src/App.jsx
@@ -1,12 +1,20 @@
 import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Details from "./Details";
+import { Link } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt me!</h1>
-      <SearchParams />
-    </div>
+    <BrowserRouter>
+      <Link style={{ textDecoration: "none" }} to="/">
+        <h1>Adopt me!</h1>
+      </Link>
+      <Routes>
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/" element={<SearchParams />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ intro-react/adopt-me/src/Details.jsx
@@ -0,0 +1,9 @@
+function Details() {
+  return (
+    <div>
+      <h2>Hi!</h2>
+    </div>
+  );
+}
+
+export default Details;
